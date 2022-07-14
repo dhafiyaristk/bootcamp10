8934039f27766e22d898915532bb8d65fb74de68
Dhafi <dhafiyarist10@gmail.com>
2022-07-14 20:33:21 +0700

    Initial commit

---
+++ src/main/java/com/bangdhaf/bootcamp10/Bootcamp10Application.java
@@ -0,0 +1,13 @@
+package com.bangdhaf.bootcamp10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bootcamp10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bootcamp10Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/bangdhaf/bootcamp10/Bootcamp10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bangdhaf.bootcamp10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bootcamp10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
